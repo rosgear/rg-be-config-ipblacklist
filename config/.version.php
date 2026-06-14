b26d70c6d4bcadad6b5a3138d0d9116d59ce50e2
Anton Tivonenko <rosgear.it@gmail.com>
2026-06-14 18:47:36 +0300

    Файл конфигурации версии расширения

---
+++ config/.version.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Этот файл является частью расширения модуля веб-приложения RosGear.
+ * 
+ * Файл конфигурации версии расширения.
+ * 
+ * @link https://rosgear.ru/
+ * @copyright Copyright (c) 2015 RosGear
+ * @license https://rosgear.ru/license/
+ */
+
+return [
+    'name'        => 'IP Blacklist',
+    'description' => 'Prevent users from accessing the control panel or site from specified IP addresses',
+    'version'     => '1.0',
+    'versionDate' => '20-12-2017',
+    'author'      => 'RosGear',
+    'authorUrl'   => 'https://rosgear.ru/',
+    'email'       => 'info@rosgear.ru',
+    'url'         => 'https://rosgear.ru/component/rg-be-config-ipblacklist/',
+    'license'     => 'The MIT License',
+    'licenseUrl'  => 'https://rosgear.ru/license/#license-mit'
+];
